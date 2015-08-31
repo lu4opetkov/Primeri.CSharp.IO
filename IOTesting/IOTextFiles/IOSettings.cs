88dba6282935105c25b29ff696f3aad9e169ee4f
Лъчезар Петков <lu4i@abv.bg>
2015-08-31 13:39:53 +0300

    Модул 08 - Видео 02 - Запис на текстов файл

---
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	public class IOSettings
+	{
+		private STable _stable;
+		public IOSettings (STable stable)
+		{
+			_stable = stable;
+		}
+		public bool save()
+		{
+			try{
+				string _temp = "";
+				_temp = string.Join("\r\n", _stable.stable);
+				System.IO.File.WriteAllText("d://aula//text.txt", _temp);
+ 				//Запис на текстов файл
+
+				return true;
+
+				}catch{
+				}
+
+		return false;
+
+		}
+	}
+}
+
---
+++ IOTesting/IOTextFiles/STable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IOTextFiles
+{
+	public class STable
+	{
+		public string[] stable = new string[4];
+		public STable ()
+		{
+			fillStable();
+		}
+		private void fillStable()
+		{
+			stable [0] = "1,0,5,0.4546";
+			stable [1] = "1,5,10,0.4546";
+			stable [2] = "1,10,15,0.4546";
+			stable [3] = "1,15,20,0.4546";
+
+
+		}
+
+	}
+}
+
