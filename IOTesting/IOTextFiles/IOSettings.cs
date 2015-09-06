83073c5ae605fe344388071d296d3805c116b433
Лъчезар Петков <lu4i@abv.bg>
2015-09-06 22:38:25 +0300

    Модул 08 - Видео 03 - Четене на текстов файл

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -9,6 +9,14 @@ namespace IOTextFiles
 		{
 			_stable = stable;
 		}
+		public string getPath()
+		{
+			string _path = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory,"test.txt");
+			return _path;
+		}
+
+
+
 		public bool save()
 		{
 			try{
@@ -24,6 +32,23 @@ namespace IOTextFiles
 
 		return false;
 
+		}
+		public bool open ()
+		{
+			try
+			{
+				string _temp = System.IO.File.ReadAllText(getPath());
+				string [] _table = _temp.Replace("\r", "").Split('\n');
+				for (int i = 0; i < _table.Length; i++ )
+				{
+					_stable.stable[i] = _table[i];
+				}
+
+
+				return true;
+			}catch{
+				}
+			return false;
 		}
 	}
 }
--- IOTesting/IOTextFiles/Program.cs
+++ IOTesting/IOTextFiles/Program.cs
@@ -10,12 +10,14 @@ namespace IOTextFiles
 		{
 			STable _STable = new STable ();
 			IOSettings _io = new IOSettings (_STable);
-			Console.WriteLine (_STable.stable[0]);
-			if (_io.save()) {
-				Console.WriteLine("Таблицата е запаметена успешно");
+
+
+			if (_io.open()) {        // _io.save()){
+				Console.WriteLine("Таблицата е прочетена успешно");
 			}else{
-				Console.WriteLine("Таблицата Не е запаметена успешно");
+				Console.WriteLine("Таблицата Не е прочетена успешно");
 			}
+			Console.WriteLine (_STable.stable[2]);
 		}
 	}
 }
